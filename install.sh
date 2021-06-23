1b6da3fece7359f042f1be85205782404e43f238
Mark Chambers <mark.chambers@sharesies.co.nz>
2021-06-23 14:28:26 +1200

    add albert

--- install.sh
+++ install.sh
@@ -28,6 +28,13 @@ sudo snap install docker
 sudo add-apt-repository universe
 sudo apt install gnome-tweak-tool
 
+# Albert (quick open tool)
+curl https://build.opensuse.org/projects/home:manuelschneid3r/public_key | sudo apt-key add -
+echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
+sudo wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_20.04/Release.key -O "/etc/apt/trusted.gpg.d/home:manuelschneid3r.asc"
+sudo apt update
+sudo apt install albert
+
 # Solaar (logitech mouse helper)
 sudo apt-get update -y
 sudo apt-get install -y solaar
